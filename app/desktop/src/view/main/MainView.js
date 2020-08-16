f77e3c22e29d3d025c67c886401c47a38d246d7f
mrBizik <bizigg@gmail.com>
2020-08-16 20:58:37 +0500

    Основное представление для приложения

--- app/desktop/src/view/main/MainView.js
+++ app/desktop/src/view/main/MainView.js
@@ -5,61 +5,54 @@ Ext.define('TaskBoard.view.main.MainView', {
   viewModel: {
     type: 'mainviewmodel'
   },
-  // layout: 'border',
+  layout: 'border',
   items: [
-    // {
-    //   xtype: 'headerview',
-    //   reference: 'headerview',
-    //   region: 'north',
-    //   docked: 'top',
-    //   weight: -2
-    // },
-    // {
-    //   xtype: 'boardview',
-    //   reference: 'boardview',
-    //   region: 'center',
-    //   width: '100%',
-    //   weight: -1,
-    //   listeners: {
-    //     select: 'onTaskSelect',
-    //   },
-    // },
-    // {
-    //   xtype: 'footerview',
-    //   reference: 'footerview',
-    //   region: 'south',
-    //   docked: 'bottom',
-    //   weight: -2
-    // },
-
     {
-      xtype: 'panel',
-      title: 'Plan',
-      cls: 'boardview-task-group',
+      xtype: 'headerview',
+      reference: 'headerview',
+      region: 'north',
+      docked: 'top',
+      weight: -2
+    },
+    {
+      xtype: 'contentview',
       region: 'center',
       weight: -1,
       items: [
-          {
-              xtype: 'dataview',
-              height: '100%',
-              itemTpl: new Ext.XTemplate(
-                  '<div class="boardview-item-task-wrap">',
-                  '<div class="item-task-base-info">',
-                  '<div class="item-task-id">{id}</div>',
-                  '<div class="item-task-name">{name}</div>',
-                  '</div>',
-                  '<div class="item-task-status-info">',
-                  '<div class="item-task-user-name">Owner: {firstName} {secondName}</div>',
-                  '<div class="item-task-priority">Priority: {priority}</div>',
-                  '</div>',
-                  '</div>',
-              ),
-              bind: {
-                  store: '{planTasks}',
-              }
-          },
-      ]
-  },
-
-  ]
+        {
+          xtype: 'panel',
+          title: 'Plan',
+          cls: 'boardview-task-group',
+          items: [
+              {
+                  xtype: 'dataview',
+                  height: '100%',
+                  itemTpl: new Ext.XTemplate(
+                      '<div class="boardview-item-task-wrap">',
+                      '<div class="item-task-base-info">',
+                      '<div class="item-task-id">{id}</div>',
+                      '<div class="item-task-name">{name}</div>',
+                      '</div>',
+                      '<div class="item-task-status-info">',
+                      '<div class="item-task-user-name">Owner: {firstName} {secondName}</div>',
+                      '<div class="item-task-priority">Priority: {priority}</div>',
+                      '</div>',
+                      '</div>',
+                  ),
+                  bind: {
+                      store: '{planTasks}',
+                  }
+              },
+          ]
+        },
+      ],
+    },
+    {
+      xtype: 'footerview',
+      reference: 'footerview',
+      region: 'south',
+      docked: 'top',
+      weight: -2
+    },
+  ],
 });
---
+++ app/desktop/src/view/main/content/ContentView.js
@@ -0,0 +1,6 @@
+Ext.define('TaskBoard.view.main.footer.ContentView', {
+	extend: 'Ext.Container',
+	xtype: 'contentview',
+	cls: 'contentview',
+	width: '100%',
+});
